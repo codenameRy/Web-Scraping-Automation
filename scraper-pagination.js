1c14332c38b08fee7958bbd4bfa4f8e6cd2b5869
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-07 00:35:42 -0500

    Pagination update

--- scraper-pagination.js
+++ scraper-pagination.js
@@ -10,7 +10,33 @@ function run (pagesToScrape) {
             await page.goto('https://news.ycombinator.com/');
             let currentPage = 1;
             let urls = [];
-            
+            while (currentPage <= pagesToScrape) {
+                let newURLs = await page.evaluate(() => {
+                    let results = [];
+                    let items = document.querySelectorAll('a.storylink');
+                    items.forEach((item) => {
+                        results.push( {
+                            url: item.getAttribute('href'),
+                            text: item.innerText,
+                        });
+                    });
+                    return results;
+                });
+                urls = urls.concat(newURLs);
+                if (currentPage > pagesToScrape) {
+                    await Promise.all([
+                        await page.click('a.morelink'),
+                        await page.waitForSelector('a.storylink')
+                    ])
+                }
+                currentPage++;
+            }
+            browser.close();
+            return resolve(urls);
+        } catch (e) {
+            return reject (e);
         }
     })
 }
+
+run(5).then(console.log).catch(console.error);
